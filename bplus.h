de59f074ccf37a349a5ae179eb771b6ab302dffb
M4tt0tito <francisca.marin01@alumnos.ucn.cl>
2025-12-06 07:16:22 -0300

    Add BPlus tree node classes

---
+++ bplus.h
@@ -0,0 +1,20 @@
+class NodoBPlusBase {
+  private:
+    int* claves; // vector de ids de NodoGrafo
+    int orden; // factor m de árbol
+  bool es_hoja; // indica si es nodo interno o hoja del árbol
+};
+
+class NodoBInterno: public NodoBPlusBase {
+  private:
+    NodoBPlusBase** punteros; // Vector del tipo NodoBPlusBase*
+    // función que dado un id de NodoGrafo indica a cual Nodo del árbol B hay
+    // que moverse. Devuelve el índice de vector punteros.
+    int buscar_siguiente();
+};
+
+class NodoBHoja: public NodoBPlusBase {
+  private:
+    NodoGrafo** datos; // vector del tipo NodoGrafo*
+    NodoBHoja* siguiente_hoja; // puntero del tipo NodoBHoja*
+};
