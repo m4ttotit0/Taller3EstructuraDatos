a9131c5945d49fd1580776e29ed7bf7071fe4dfc
M4tt0tito <francisca.marin01@alumnos.ucn.cl>
2025-12-06 07:26:05 -0300

    Add file system menu in main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "system.h"
+
+void menu() {
+  std::cout << "\n Sistema de Archivos \n";
+  std::cout << "1. Crear archivo\n";
+  std::cout << "2. Eliminar archivo\n";
+  std::cout << "3. Listar contenido de directorio\n";
+  std::cout << "4. Buscar archivo por id\n";
+  std::cout << "5. Obtener rutas completas\n";
+  std::cout << "6. Calcular espacio ocupado\n";
+  std::cout << "9. Salir\n";
+  std::cout << "Ingrese su opcion: ";
+} 
+
+int main() {
+}
