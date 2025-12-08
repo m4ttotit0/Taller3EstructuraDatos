d5dba12fe9f3e8b4aa987991a550dba507f98dfc
M4tt0tito <francisca.marin01@alumnos.ucn.cl>
2025-12-08 13:24:17 -0300

    Initialize system with includes and variables
    Added includes and variable declarations for system functionality.

--- system.cpp
+++ system.cpp
@@ -1,1 +1,11 @@
-//sistema implementación
+#include <iostream>
+#include "system.h"
+
+int next = 1;
+long  access= 0;
+ArbolBPlus index;
+int numRutes = 0;
+
+NodoGrafo* buscar_nodo_grafo(int clave) {
+}
+
