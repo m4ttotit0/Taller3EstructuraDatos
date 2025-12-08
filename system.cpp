83ab87d24f194ad9fb06ece3bfbbfa416106dfc9
M4tt0tito <francisca.marin01@alumnos.ucn.cl>
2025-12-08 13:26:20 -0300

    Refactor node graph functions and variable naming
    Refactor node graph functions and update variable names.

--- system.cpp
+++ system.cpp
@@ -4,8 +4,25 @@
 int next = 1;
 long  access= 0;
 ArbolBPlus index;
-int numRutes = 0;
+int numRute = 0;
 
-NodoGrafo* buscar_nodo_grafo(int clave) {
+void insertar_nodo_grafo(int clave, NodoGrafo* nodo_grafo){
+}
+
+NodoGrafo* buscar_nodo_grafo(int clave){
+}
+
+void crear_nodo(int id_padre){
+}
+
+void eliminar_archivo(int id_archivo, int id_directorio_padre){
+}
+
+void listar_contenido(int id_directorio){
+}
+string* obtener_rutas_completas(int id_archivo){
+}
+
+int calcular_espacio_ocupado(int id_directorio){
 }
 
