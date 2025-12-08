f53437a3c3b64742f3b1d98c82a0a4449935c2a7
M4tt0tito <francisca.marin01@alumnos.ucn.cl>
2025-12-08 13:22:46 -0300

    Add header guards and function declarations in system.h

--- system.h
+++ system.h
@@ -1,1 +1,19 @@
-//sistema
+#ifndef system.h
+#define system.h
+
+#include <string>
+#include "nodo_grafo.h"
+#include "bplus.h"
+
+extern int next;
+extern long access;
+extern ArbolBPlus index;
+extern int numRute;
+
+void insertar_nodo_grafo(int clave, NodoGrafo* nodo_grafo);
+NodoGrafo* buscar_nodo_grafo(int clave);
+void crear_nodo(int id_padre);
+void eliminar_archivo(int id_archivo, int id_directorio_padre);
+void listar_contenido(int id_directorio);
+string* obtener_rutas_completas(int id_archivo);
+int calcular_espacio_ocupado(int id_directorio);
