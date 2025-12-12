1afc9032ab1020c572cc181a4eb82df9651c230e
M4tt0tito <francisca.marin01@alumnos.ucn.cl>
2025-12-12 14:01:20 -0300

    Refactor BPlus tree node classes and access modifiers

--- bplus.h
+++ bplus.h
@@ -1,23 +1,70 @@
 #pragma once
-#include "NodoGrafo.h"
+#include " NodoGrafo .h"
+
+const int ORDEN_BPLUS = 4;
 
 class NodoBPlusBase {
-  private:
-    int* claves; // vector de ids de NodoGrafo
-    int orden; // factor m de árbol
-  bool es_hoja; // indica si es nodo interno o hoja del árbol
+protected:
+    int* claves;
+    int cuenta;
+    int orden;
+    bool es_hoja;
+
+public:
+    NodoBPlusBase(int _orden, bool _es_hoja);
+    virtual ~NodoBPlusBase();
+
+    bool esHoja() const;
+    int getCuenta() const;
+    int getClave(int i) const;
+    void setClave(int i, int v);
+    void setCuenta(int c);
+
+    friend class ArbolBPlus;
 };
 
-class NodoBInterno: public NodoBPlusBase {
-  private:
-    NodoBPlusBase** punteros; // Vector del tipo NodoBPlusBase*
-    // función que dado un id de NodoGrafo indica a cual Nodo del árbol B hay
-    // que moverse. Devuelve el índice de vector punteros.
-    int buscar_siguiente();
+class NodoBInterno : public NodoBPlusBase {
+    NodoBPlusBase** punteros;
+
+public:
+    NodoBInterno(int _orden);
+    ~NodoBInterno();
+
+    int buscar_siguiente(int clave);
+
+    NodoBPlusBase* getPuntero(int i);
+    void setPuntero(int i, NodoBPlusBase* p);
+
+    friend class ArbolBPlus;
 };
 
-class NodoBHoja: public NodoBPlusBase {
-  private:
-    NodoGrafo** datos; // vector del tipo NodoGrafo*
-    NodoBHoja* siguiente_hoja; // puntero del tipo NodoBHoja*
+class NodoBHoja : public NodoBPlusBase {
+    NodoGrafo** datos;
+    NodoBHoja* siguiente_hoja;
+
+public:
+    NodoBHoja(int _orden);
+    ~NodoBHoja();
+
+    NodoBHoja* getSiguiente();
+    void setSiguiente(NodoBHoja* s);
+
+    friend class ArbolBPlus;
+};
+
+class ArbolBPlus {
+    NodoBPlusBase* raiz;
+    int orden;
+
+    NodoBHoja* insertar_en_hoja(NodoBHoja* hoja, int clave,
+                                NodoGrafo* valor, int& outClavePromovida);
+    NodoBPlusBase* insertar_rec(NodoBPlusBase* nodo, int clave,
+                                NodoGrafo* valor, int& outClavePromovida);
+
+public:
+    ArbolBPlus(int _orden = ORDEN_BPLUS);
+
+    void insertar(int clave, NodoGrafo* valor);
+    NodoGrafo* buscar(int clave);
+    void eliminar(int clave);
 };
