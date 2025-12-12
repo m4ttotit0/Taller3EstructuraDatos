f3f32465018abc1b71991a105837830ad100c76c
M4tt0tito <francisca.marin01@alumnos.ucn.cl>
2025-12-12 14:37:47 -0300

    Implement root node creation and menu options
    Added root node creation and menu loop in main function.

--- main.cpp
+++ main.cpp
@@ -14,4 +14,31 @@ void menu() {
 } 
 
 int main() {
+  // Crear raíz
+    int id_root = nextId++;
+    NodoDirectorio* root = new NodoDirectorio(id_root, "root");
+    insertar_nodo_grafo(id_root, root);
+
+    int opcion;
+    do {
+        mostrar_menu();
+        std::cin >> opcion;
+
+        if (opcion == 1) {
+            //Llamar función crear archivo
+        } else if (opcion == 2) {
+          //eliminar archivo
+        } else if (opcion == 3) {
+          //lista contenido
+        } else if (opcion == 4) {
+          //buscar arch por id
+        } else if (opcion == 5) {
+          //rutas completas
+        } else if (option == 6){
+          //calcular espacio
+	      }
+	} while(opcion != 9);
+
+	return 0;
 }
+
